eab9f083b5c6a9c9b10532dd7533e75240fadaf8
Candler <candlersartain@gmail.com>
2017-05-20 18:39:59 -0400

    Added command to return market data from a specified currency pair and exchange.

--- poloniex.py
+++ poloniex.py
@@ -1,7 +1,8 @@
 import urllib.request, json
 
+# Returns all Poloniex currency pairs
 def getCurrencyPairs():
-    return ["BTC_BCN", "BTC_BELA", "BTC_BLK", "BTC_BTCD", "BTC_BTM", "BTC_BTS", "BTC_BURST", "BTC_GNT", 
+    btc = ["BTC_BCN", "BTC_BELA", "BTC_BLK", "BTC_BTCD", "BTC_BTM", "BTC_BTS", "BTC_BURST", "BTC_GNT", 
     "BTC_CLAM", "BTC_DASH", "BTC_DGB", "BTC_DOGE", "BTC_EMC2", "BTC_FLDC", "BTC_FLO", "BTC_GAME", "BTC_GRC", 
     "BTC_HUC", "BTC_LTC", "BTC_MAID", "BTC_OMNI", "BTC_NAUT", "BTC_NAV", "BTC_NEOS", "BTC_NMC", "BTC_NOTE", 
     "BTC_NXT", "BTC_PINK", "BTC_POT", "BTC_PPC", "BTC_RIC", "BTC_SJCX", "BTC_STR", "BTC_SYS", "BTC_VIA", 
@@ -9,22 +10,54 @@ def getCurrencyPairs():
     "BTC_ETH", "BTC_SC", "BTC_BCY", "BTC_EXP", "BTC_FCT", "BTC_RADS", "BTC_AMP", "BTC_DCR", "BTC_LSK", "BTC_LBC", 
     "BTC_STEEM", "BTC_SBD", "BTC_ETC", "BTC_REP", "BTC_ARDR", "BTC_ZEC", "BTC_STRAT", "BTC_NXC", "BTC_PASC"]
 
-def getCurrencyPair(coin, coinPair):
+    #TODO - Add other currency pairs
+    eth = []
+    xmr = []
+    usdt = []
+
+    return btc + eth + xmr + usdt
+
+# Returns the coin name for the given symbol
+def getReadableCoinName(coin):
+    names = {"BCN": "Bytecoin", "BELA": "Belacoin", "BLK": "BlackCoin", "BTCD": "BitcoinDark", "BTM": "Bitmark", "BTS": "BitShares", 
+    "BURST": "Burst", "GNT": "Golem", "CLAM": "CLAMS", "DASH": "Dash", "DGB": "DigiByte", "DOGE": "Dogecoin", "EMC2": "Einsteinium", 
+    "FLDC": "FoldingCoin", "FLO": "Florincoin", "GAME": "GameCredits", "GRC": "Gridcoin Research", "HUC": "Huntercoin", 
+    "LTC": "Litecoin", "MAID": "MaidSafeCoin", "OMNI": "Omni", "NAUT": "Nautiluscoin", "NAV": "NAVCoin", "NEOS": "Neoscoin", 
+    "NMC": "Namecoin", "NOTE": "DNotes", "NXT": "NXT", "PINK": "Pinkcoin", "POT": "PotCoin", "PPC": "Peercoin", "RIC": "Riecoin", 
+    "SJCX": "Storjcoin X", "STR": "Stellar", "SYS": "Syscoin", "VIA": "Viacoin", "XVC": "Vcash", "VRC": "VeriCoin", "VTC": "Vertcoin",
+    "XBC": "BitcoinPlus", "XCP": "Counterparty", "XEM": "NEM", "XMR": "Monero", "XPM": "Primecion", "XRP": "Ripple", "GNO": "Gnosis",
+    "ETH": "Ethereum", "SC": "Siacoin", "BCY": "BitCrystals", "EXP": "Expanse", "FCT": "Factom", "RADS": "Radium", "AMP": "Synereo AMP",
+    "DCR": "Decred", "LSK": "Lisk", "LBC": "LBRY Credits", "STEEM": "STEEM", "SBD": "Steem Dollars", "ETC": "Ethereum Classic",
+    "REP": "Augur", "ARDR": "Ardor", "ZEC": "Zcash", "STRAT": "Stratis", "NXC": "Nexium", "PASC": "PascalCoin"}
+
+    return names[coin]
+
+# Returns the currency pair for the given symbol
+def getCurrencyPair(coin, coinPair = "BTC"):
     pairs = getCurrencyPairs()
     pair = coinPair + "_" + coin
     if pair in pairs:
         return pair
+    else:
+        return None
 
-    return None
-
+# Returns ticker data from Poloniex for the given currency pair
 def getTickerData(pair):
     url = "https://poloniex.com/public?command=returnTicker"
-    with urllib.request.urlopen(url) as url:
-        ticker = json.loads(url.read().decode())
-        return ticker[pair]
+    pairs = getCurrencyPairs()
 
-def getTickerMessage(ticker, coin):
-    coin = "**COIN NAME (" + coin + ")**\n"
+    if pair in pairs:
+        with urllib.request.urlopen(url) as url:
+            ticker = json.loads(url.read().decode())
+            return ticker[pair]
+    else:
+        return None
+
+# Returns formatted market data for bot to send
+# TODO - Attempt to implement using discord.ext formatter
+def getTickerMessage(ticker, pair):
+    coin = getReadableCoinName(pair.split("_")[1])
+    header = "**" + coin + " (" + pair + ") - Poloniex**\n"
     seperator = "-----------------------\n"
     price = "Current Price: `" + ticker["last"] + "`\n"
     high = "24hr High: `" + ticker["high24hr"] + "`\n"
@@ -35,6 +68,6 @@ def getTickerMessage(ticker, coin):
     sign = "+" if changeNum > 0 else ""
     change = "Percent Change: ```diff\n" + sign + str(changeNum) + "%\n```"
 
-    message = coin + seperator + price + volume + high + low + change
+    message = header + seperator + price + volume + high + low + change
 
     return message
--- satoshi.py
+++ satoshi.py
@@ -10,24 +10,53 @@ pairs = poloniex.getCurrencyPairs()
 async def on_ready():
     print('Satoshi Online!')
     print('---------------')
-    # Include logging here
+    # Include logging here    
 
 @client.event
 async def on_message(message):
-    msg = message.content
+    msg = message.content.lower()
+    channel = message.channel
     words = msg.split()
-    calls = 0
-    for word in words:
-        if calls >= 3:
-            await client.send_message(message.channel, "You can only request data for up to 3 coins per message")
-            break
-        if word.startswith("$"):
-            coin = word[1:]
-            pair = poloniex.getCurrencyPair(coin, "BTC")
-            if pair:
-                ticker = poloniex.getTickerData(pair)
-                tickerMessage = poloniex.getTickerMessage(ticker, coin)
-                await client.send_message(message.channel, tickerMessage)
-                calls += 1
+
+    # Check if a possible command has been submitted
+    if msg.startswith('+'):
+        cmd = words[0][1:]
+        if cmd == "coin":
+            try:
+                pair = words[1].upper()
+                exchange = words[2]
+            except:
+                await client.send_message(channel, "The `$coin` command must be formatted like this: `$coin <currency_pair> <exchange>`.\n\n For example: `$coin BTC_ETH poloniex`.")
+                return
+
+            response = cmdCoin(pair, exchange)
+
+        if response:
+            await client.send_message(channel, response)
+
+    # If no command, parse message for currency requests
+    else:
+        calls = 0
+        for word in words:
+            if calls >= 3:
+                await client.send_message(channel, "You can only request data for up to 3 coins per message.")
+                break
+            if word.startswith("$"):
+                coin = word[1:].upper()
+                pair = poloniex.getCurrencyPair(coin)
+                if pair:
+                    ticker = poloniex.getTickerData(pair)
+                    tickerMessage = poloniex.getTickerMessage(ticker, pair)
+                    calls += 1
+                    await client.send_message(channel, tickerMessage)                    
+
+# Returns current market data for the specified currency pair and exchange
+def cmdCoin(pair, exchange):
+    if exchange == 'poloniex':
+        ticker = poloniex.getTickerData(pair)
+        if ticker:
+            return poloniex.getTickerMessage(ticker, pair)
+    
+    return "The currency pair `" + pair + "` was not found on `" + exchange + "`. Please try again."
 
 client.run(sys.argv[1])
