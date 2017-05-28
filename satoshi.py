d23e6444631b441377b024f2c082732884e0364b
Candler <candlersartain@gmail.com>
2017-05-28 18:17:09 -0400

    Added $BTC and +bitcoin commands

--- satoshi.py
+++ satoshi.py
@@ -1,7 +1,7 @@
 import discord
-from discord.ext import commands
 import asyncio
 import poloniex
+import coindesk
 import sys
 
 client = discord.Client()
@@ -21,22 +21,34 @@ async def on_message(message):
 
     # Check if a possible command has been submitted
     if msg.startswith('+'):
+        response = None
         cmd = words[0][1:]
         if cmd == "coin":
             try:
                 pair = words[1].upper()
                 exchange = words[2]
             except:
-                await client.send_message(channel, "The `$coin` command must be formatted like this: `$coin <currency_pair> <exchange>`.\n\n For example: `$coin BTC_ETH poloniex`.")
+                await client.send_message(channel, "The `+coin` command must be formatted like this: `+coin <currency_pair> <exchange>`.\n\n For example: `+coin BTC_ETH poloniex`")
                 return
 
             response = cmdCoin(pair, exchange)
+
+        if cmd == "bitcoin":
+            try:
+                currency = words[1].upper()
+            except:
+                await client.send_message(channel, "The `+bitcoin` command must be formatted like this: `+bitcoin <currency>`.\n\n For example: `+bitcoin GBP`")
+                return
+
+            response = cmdBitcoin(currency)
 
         if cmd == "help":
             response = cmdHelp()
 
         if response:
             await client.send_message(channel, response)
+        else:
+            await client.send_message(channel, "`+" + cmd + "` is not a valid command. Type `+help` to get a list of valid commands.")
 
     # If no command, parse message for currency requests
     else:
@@ -48,6 +60,11 @@ async def on_message(message):
             if word.startswith("$"):
                 coin = word[1:].upper()
                 pair = poloniex.getCurrencyPair(coin)
+
+                if coin == "BTC":
+                    ticker = coindesk.getTickerData()
+                    tickerMessage = coindesk.getTickerMessage(ticker)
+                    await client.send_message(channel, tickerMessage)
                 if pair:
                     ticker = poloniex.getTickerData(pair)
                     tickerMessage = poloniex.getTickerMessage(ticker, pair)
@@ -63,18 +80,27 @@ def cmdCoin(pair, exchange):
     
     return "The currency pair `" + pair + "` was not found on `" + exchange + "`. Please try again."
 
+# Returns current market data for bitcoin
+def cmdBitcoin(currency):
+    ticker = coindesk.getTickerData(currency)
+    if isinstance(ticker, dict):
+        return coindesk.getTickerMessage(ticker)
+    else:
+        return ticker
+
 # Returns help message
 def cmdHelp():
     intro = "Thank you for using Satoshi! This bot provides real time market data for cryptocurrencies on multiple exchanges.\n\n"
     exchanges = "__**Supported Exchanges**__\n\nThe current supported exchanges are: `Poloniex`\n\n"
     commands = "__**Commands**__\n\n"
-    cmdCoin = "`+coin <currency pair> <exchange>` - Returns market data for the specified coin/exchange. Example: `+coin BTC_LTC Poloniex`\n\n"
+    cmdCoin = "`+coin <currency pair> <exchange>` - Returns market data for the specified coin/exchange.\nExample: `+coin BTC_LTC Poloniex`\n\n"
+    cmdBitcoin = "`+bitcoin <currency>` - Returns current bitcoin price for the specified currency.\nExample: `+bitcoin GBP`\n\n"
     cmdHelp = "`+help` - Returns information about Satoshi\n\n"
     lookup = """You may also retrieve data for up to three coins by simply including `$coin` anywhere in your message. 
 If a coin is present on mutiple exchanges, data will be returned from Poloniex. If you wish to specify the exchange, use the `+coin` command. 
 Example: `Wow, look at $ETC!`\n\n"""
     github = "__**Github**__\n\nThis project can be found on Github at `https://github.com/cmsart/Satoshi`"
 
-    return intro + exchanges + commands + cmdCoin + cmdHelp + lookup.replace("\t", "") + github
+    return intro + exchanges + commands + cmdCoin + cmdBitcoin + cmdHelp + lookup.replace("\t", "") + github
 
 client.run(sys.argv[1])
